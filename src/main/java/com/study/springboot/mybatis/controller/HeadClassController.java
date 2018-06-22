25a3037bed25984054e18172a2d42940624f0f2a
DS-Chen <tsuchen@outlook.com>
2018-06-22 15:07:50 +0800

    修改bug

--- src/main/java/com/study/springboot/mybatis/controller/HeadClassController.java
+++ src/main/java/com/study/springboot/mybatis/controller/HeadClassController.java
@@ -23,7 +23,8 @@ public class HeadClassController {
 
     @Autowired
     private HeadClassMapper headClassMapper;
-    @RequestMapping(value = "/classInfo",method = RequestMethod.GET,produces = {"application/json"})
+
+    @RequestMapping(value = "/getClassInfo",method = RequestMethod.GET,produces = {"application/json"})
     public DataResponseVo getClassInfo(@RequestParam(value = "id",required = true) Integer id){
         return new DataResponseVo(0,headClassMapper.selectClassById(id));
     }
