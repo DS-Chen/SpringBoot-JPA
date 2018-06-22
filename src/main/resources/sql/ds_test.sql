d1685250dc5c97dc2c45c67787663861f03b9ba9
DS-Chen <tsuchen@outlook.com>
2018-06-22 15:10:13 +0800

    修改数据库字段

--- src/main/resources/sql/ds_test.sql
+++ src/main/resources/sql/ds_test.sql
@@ -1,38 +1,40 @@
 -- 学生表
 create table student(
-  id int primary key auto_increment,
-  name varchar(20),
-  sex varchar(8),
-  age int,
-  c_id int,
-  foreign key(c_id) references headclass(hc_id)
+  s_id int primary key auto_increment,
+  s_name varchar(20),
+  s_sex varchar(8),
+  s_age int,
+  s_cid int,
+  foreign key(s_cid) references headclass(hc_id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8
-insert into student(name,sex,age,c_id) values('tom','男',20,1);
-insert into student(name,sex,age,c_id) values('jack','男',18,2);
-insert into student(name,sex,age,c_id) values('chen','男',21,1);
+insert into student(s_name,s_sex,s_age,s_cid) values('tom','男',20,1);
+insert into student(s_name,s_sex,s_age,s_cid) values('jack','男',18,2);
+insert into student(s_name,s_sex,s_age,s_cid) values('chen','男',21,1);
 -- 课程表
 create table course(
-id int primary key auto_increment,
-name varchar(20),
-credit int
+c_id int primary key auto_increment,
+c_name varchar(20),
+c_credit int
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8
-insert into course(name,credit) values('Math',5);
-insert into course(name,credit) values('Computer',3);
+insert into course(c_name,c_credit) values('Math',5);
+insert into course(c_name,c_credit) values('Computer',3);
 
 -- 学生课程中间表
 create table student_course(
- s_id int,
- c_id int,
+ sc_sid int,
+ sc_cid int,
  sc_date date,
- primary key(s_id,c_id),
- foreign key(s_id) references student(id),
- foreign key(c_id) references course(id)
+ primary key(sc_sid,sc_cid),
+ foreign key(sc_sid) references student(s_id),
+ foreign key(sc_cid) references course(c_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-insert into student_course(s_id,c_id,sc_date) values(1,1,'2018-01-01');
-insert into student_course(s_id,c_id,sc_date) values(1,2,'2018-01-02');
-insert into student_course(s_id,c_id,sc_date) values(2,1,'2018-02-01');
-insert into student_course(s_id,c_id,sc_date) values(2,2,'2018-02-02');
+insert into student_course(sc_sid,sc_cid,sc_date) values(1,1,'2018-01-01');
+insert into student_course(sc_sid,sc_cid,sc_date) values(1,2,'2018-01-02');
+insert into student_course(sc_sid,sc_cid,sc_date) values(2,1,'2018-02-01');
+insert into student_course(sc_sid,sc_cid,sc_date) values(2,2,'2018-02-02');
+insert into student_course(sc_sid,sc_cid,sc_date) values(3,1,'2018-02-05');
+insert into student_course(sc_sid,sc_cid,sc_date) values(3,2,'2018-02-06');
 
 -- 班主任表
 create table headtecher(
@@ -55,12 +57,5 @@ create table headclass(
 insert into headclass(hc_name,ht_id) values('软件工程（1）班',1);
 insert into headclass(hc_name,ht_id) values('软件工程（2）班',2)
 
-select * from
-student S
-left join student_course SC on S.id=SC.s_id
-left join course C on SC.c_id=C.id
-left join headclass HC on S.c_id=HC.hc_id
-left join headtecher HT on HC.ht_id=HT.ht_id
-where S.id=1
 
 
